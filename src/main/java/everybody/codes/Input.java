285c27af8045200234d5ab7d9bd760e26b3c1337
Stanislav Schmidt <staschmidt@expediagroup.com>
2024-11-09 23:26:34 +0100

    Solve q2p1

--- src/main/java/everybody/codes/Input.java
+++ src/main/java/everybody/codes/Input.java
@@ -3,10 +3,19 @@ package everybody.codes;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class Input {
     public static String read(int quest, int part) throws IOException {
+        return Files.readString(makePath(quest, part));
+    }
+
+    public static List<String> readLines(int quest, int part) throws IOException {
+        return Files.readAllLines(makePath(quest, part));
+    }
+
+    private static Path makePath(int quest, int part) {
         String path = String.format("input/everybody_codes_e2024_q%d_p%d.txt", quest, part);
-        return Files.readString(Path.of(path));
+        return Path.of(path);
     }
 }
--- src/main/java/everybody/codes/Quest2.java
+++ src/main/java/everybody/codes/Quest2.java
@@ -1,11 +1,33 @@
 package everybody.codes;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Quest2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Quest2 quest = new Quest2();
+        List<String> input = Input.readLines(2, 1);
+        String[] words = input.get(0).replaceFirst("WORDS:", "").split(",");
+        int solution = quest.countRunicWords(words, input.get(2));
+        System.out.println("Part 1: " + solution);
     }
 
     public int countRunicWords(String[] words, String inscription) {
-        return 0;
+        return Stream.of(words)
+            .map(word -> countOccurrences(word, inscription))
+            .flatMapToInt(IntStream::of)
+            .sum();
+    }
+
+    private int countOccurrences(String token, String phrase) {
+        int count = 0;
+        int idx = 0;
+        while ((idx = phrase.indexOf(token, idx)) != -1) {
+            count += 1;
+            idx += token.length();
+        }
+        return count;
     }
 }
