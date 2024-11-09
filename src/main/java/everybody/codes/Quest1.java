d2b83628301d39e1d5a550d0c56d33dcd235bde5
Stanislav Schmidt <staschmidt@expediagroup.com>
2024-11-09 22:37:16 +0100

    Rename argv to args

--- src/main/java/everybody/codes/Quest1.java
+++ src/main/java/everybody/codes/Quest1.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class Quest1 {
-    public static void main(String[] argv) throws IOException {
+    public static void main(String[] args) throws IOException {
         Quest1 quest = new Quest1();
         for (int part : List.of(1, 2, 3)) {
             String input = Input.read(1, part);
