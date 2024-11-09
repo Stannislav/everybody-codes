84aa3f87893644d789a750300c92f294ec93bb74
Stanislav Schmidt <staschmidt@expediagroup.com>
2024-11-09 22:47:27 +0100

    Add stub for q2p1

--- src/main/java/everybody/codes/Quest2.java
+++ src/main/java/everybody/codes/Quest2.java
@@ -4,4 +4,8 @@ public class Quest2 {
     public static void main(String[] args) {
         Quest2 quest = new Quest2();
     }
+
+    public int countRunicWords(String[] words, String inscription) {
+        return 0;
+    }
 }
--- src/test/java/everybody/codes/Quest2Test.java
+++ src/test/java/everybody/codes/Quest2Test.java
@@ -1,10 +1,21 @@
 package everybody.codes;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class Quest2Test {
+    private final Quest2 quest = new Quest2();
+
     @Test
-    public void testPart1() {}
+    public void testPart1() {
+        String[] words = {"THE", "OWE", "MES", "ROD", "HER"};
+
+        assertEquals(4, quest.countRunicWords(words, "AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE"));
+        assertEquals(3, quest.countRunicWords(words, "THE FLAME SHIELDED THE HEART OF THE KINGS"));
+        assertEquals(2, quest.countRunicWords(words, "POWE PO WER P OWE R"));
+        assertEquals(3, quest.countRunicWords(words, "THERE IS THE END"));
+    }
 
     @Test
     public void testPart2() {}
