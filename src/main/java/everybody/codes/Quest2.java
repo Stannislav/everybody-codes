b37a305465da27ad5858efee37d37a62b7fef3da
Stanislav Schmidt <staschmidt@expediagroup.com>
2024-11-10 00:01:43 +0100

    Solve q2p2

--- src/main/java/everybody/codes/Quest2.java
+++ src/main/java/everybody/codes/Quest2.java
@@ -1,17 +1,35 @@
 package everybody.codes;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Quest2 {
     public static void main(String[] args) throws IOException {
         Quest2 quest = new Quest2();
-        List<String> input = Input.readLines(2, 1);
-        String[] words = input.get(0).replaceFirst("WORDS:", "").split(",");
-        int solution = quest.countRunicWords(words, input.get(2));
+
+        quest.part1(Input.readLines(2, 1));
+        quest.part2(Input.readLines(2, 2));
+    }
+
+    private void part1(List<String> inputLines) {
+        String[] words = inputLines.getFirst().replaceFirst("WORDS:", "").split(",");
+        int solution = countRunicWords(words, inputLines.get(2));
         System.out.println("Part 1: " + solution);
+    }
+
+    private void part2(List<String> inputLines) {
+        String[] words = inputLines.getFirst().replaceFirst("WORDS:", "").split(",");
+        int total = 0;
+        for (String line : inputLines.subList(2, inputLines.size())) {
+            total += countRunicSymbols(words, line);
+        }
+
+        System.out.println("Part 2: " + total);
     }
 
     public int countRunicWords(String[] words, String inscription) {
@@ -19,6 +37,31 @@ public class Quest2 {
             .map(word -> countOccurrences(word, inscription))
             .flatMapToInt(IntStream::of)
             .sum();
+    }
+
+    public int countRunicSymbols(String[] words, String inscription) {
+        HashSet<Integer> symbolPositions = new HashSet<>();
+        for (String word : words) {
+            symbolPositions.addAll(getSymbolPositions(word, inscription));
+
+            String antiWord = new StringBuilder(word).reverse().toString();
+            symbolPositions.addAll(getSymbolPositions(antiWord, inscription));
+        }
+
+        return symbolPositions.size();
+    }
+
+    private Set<Integer> getSymbolPositions(String word, String inscription) {
+        Set<Integer> symbolPositions = new HashSet<>();
+        int idx = 0;
+        while ((idx = inscription.indexOf(word, idx)) != -1) {
+            for (int i = 0; i < word.length(); i++) {
+                symbolPositions.add(idx + i);
+            }
+            idx += 1;
+        }
+
+        return symbolPositions;
     }
 
     private int countOccurrences(String token, String phrase) {
--- src/test/java/everybody/codes/Quest2Test.java
+++ src/test/java/everybody/codes/Quest2Test.java
@@ -18,7 +18,14 @@ public class Quest2Test {
     }
 
     @Test
-    public void testPart2() {}
+    public void testPart2() {
+        String[] words = {"THE", "OWE", "MES", "ROD", "HER"};
+
+        assertEquals(15, quest.countRunicSymbols(words, "AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE"));
+        assertEquals(9, quest.countRunicSymbols(words, "THE FLAME SHIELDED THE HEART OF THE KINGS"));
+        assertEquals(6, quest.countRunicSymbols(words, "POWE PO WER P OWE R"));
+        assertEquals(7, quest.countRunicSymbols(words, "THERE IS THE END"));
+    }
 
     @Test
     public void testPart3() {}
