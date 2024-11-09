06f99f5a8bcd43a76daaed85f4756c2c2d510990
Stanislav Schmidt <staschmidt@expediagroup.com>
2024-11-09 22:38:23 +0100

    Add stubs for quest 2

---
+++ src/main/java/everybody/codes/Quest2.java
@@ -0,0 +1,7 @@
+package everybody.codes;
+
+public class Quest2 {
+    public static void main(String[] args) {
+        Quest2 quest = new Quest2();
+    }
+}
---
+++ src/test/java/everybody/codes/Quest2Test.java
@@ -0,0 +1,14 @@
+package everybody.codes;
+
+import org.junit.Test;
+
+public class Quest2Test {
+    @Test
+    public void testPart1() {}
+
+    @Test
+    public void testPart2() {}
+
+    @Test
+    public void testPart3() {}
+}
